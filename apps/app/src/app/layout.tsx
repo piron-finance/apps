2ab399139642d947b809e9659f5b5165f0443436
Ernest Nnamdi <ernestnamdi66@gmail.com>
2026-04-10 15:24:48 +0100

    added analytics

--- apps/app/src/app/layout.tsx
+++ apps/app/src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
         className={`${inter.variable} ${jetbrainsMono.variable} antialiased`}
       >
         <Providers>{children}</Providers>
+         <Analytics/>
       </body>
     </html>
   );
